f4c5901a238f497bd11f22d9f6c5e6bf308a2327
Meet Shah <meetdshah007@gmail.com>
2019-02-19 13:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function whisper(str){
   return str.toLowerCase();
 }
 
-//
+//'receives one argument and returns it in all lowercase'
 function logShot(str){
   console.log(str);
 }
 
+//
 function logWhisper(str){
   console.log(str);
 }
